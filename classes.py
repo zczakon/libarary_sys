7e02d7e6c6b57187bb7354364fd153f7842338a4
zczakon <56118290+zczakon@users.noreply.github.com>
2020-08-06 13:35:16 +0200

    full class implementation for library sys - class Lists

--- classes.py
+++ classes.py
@@ -140,3 +140,31 @@ class BookLending:
 
 def generate_pass(n: int) -> str:
     return ''.join((random.choice(string.ascii_letters + string.digits) for _ in range(n)))
+
+
+class Lists:
+    def __init__(self):
+        self.student_list = []
+        self.book_list = []
+
+    def add_student(self, student):
+        self.student_list.append(student)
+        pass
+
+    def del_student(self, student):
+        self.student_list.remove(student)
+        pass
+
+    def add_book(self, book):
+        self.book_list.append(book)
+        pass
+
+    def del_book(self, book):
+        self.book_list.remove(book)
+        pass
+
+    def get_student_list(self):
+        return self.student_list
+
+    def get_book_list(self):
+        return self.book_list
