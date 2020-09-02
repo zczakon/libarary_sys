fee095b770cb2582da01a16d39c762c88387dbc9
zczakon <56118290+zczakon@users.noreply.github.com>
2020-09-02 14:43:28 +0200

    changed pony queries in data_source, removed all getters and setters

--- domain_objects.py
+++ domain_objects.py
@@ -1,59 +1,41 @@
 import datetime
 
-import data_source
-from accounts import Account, Role  # TODO make method creating student acc
-from pony.orm import *
+from accounts import Account, Role
+from data_source import db
+from pony.orm import Required, Optional, Set
 
 
-class Student(data_source.db.Entity):
+class Student(db.Entity):
     name = Required(str)
     surname = Required(str)
-    pesel = Required(str, unique=True)
-    registration_date = Required(datetime)
+    pesel = Required(str)
+    registration_date = Optional(datetime.date)
     account = Optional(Account)
+    book_lendings = Set('BookLending')
 
-    fullname = str(name) + ' ' + str(surname)
+    def fullname(self):
+        return str(self.name) + ' ' + str(self.surname)
 
     def create_account(self):
-        self.account = Account(Role("user"))
+        self.account = Account(Role(name="user"))
+
+    def create(self):
+        self.registration_date = datetime.date.today()
 
     def __str__(self):
-        return '({}, {}, {}, {})'.format(self.name + ' ' + self.surname, 'PESEL: ' + str(self.pesel),
-                                         'ID:' + str(self.id), 'role: ' + str(self.account.get_role()))
+        return '({}, {}, {})'.format(self.name + ' ' + self.surname, 'PESEL: ' + str(self.pesel),
+                                     'ID:' + str(self.id)), 'role: ' + str(self.account.get_role())
 
     def __repr__(self):
-        return '({}, {}, {}, {})'.format(self.name + ' ' + self.surname, 'PESEL: ' + str(self.pesel),
-                                         'ID:' + str(self.id), 'role: ' + str(self.account.get_role()))
-
-    def get_name(self):
-        return self.name
-
-    def get_surname(self):
-        return self.surname
-
-    def get_pesel(self):
-        return self.pesel
-
-    def get_id(self):
-        return self.id
-
-    def get_reg_date(self):
-        return self.registration_date
-
-    def set_name(self, new_name):
-        self.name = new_name
-
-    def set_surname(self, new_surname):
-        self.surname = new_surname
-
-    def set_pesel(self, new_pesel):
-        self.pesel = new_pesel
+        return '({}, {}, {})'.format(self.name + ' ' + self.surname, 'PESEL: ' + str(self.pesel),
+                                     'ID:' + str(self.id)), 'role: ' + str(self.account.get_role())
 
 
-class Book(data_source.db.Entity):
+class Book(db.Entity):
     isbn = Required(str)
     title = Required(str)
     author = Required(str)
+    book_lendings = Set('BookLending')
 
     def __str__(self):
         return '({}, {}, {}, {})'.format(str(self.title), 'author: ' + self.author, 'ISBN: ' +
@@ -63,34 +45,13 @@ class Book(data_source.db.Entity):
         return '({}, {}, {}, {})'.format(str(self.title), 'author: ' + self.author, 'ISBN: ' +
                                          str(self.isbn), 'ID: ' + str(self.id))
 
-    def get_isbn(self):
-        return self.isbn
 
-    def get_title(self):
-        return self.title
-
-    def get_author(self):
-        return self.author
-
-    def get_id(self):
-        return self.id
-
-    def set_title(self, new_title):
-        self.title = new_title
-
-    def set_author(self, new_author):
-        self.author = new_author
-
-    def set_isbn(self, new_isbn):
-        self.isbn = new_isbn
-
-
-class BookLending(data_source.db.Entity):
+class BookLending(db.Entity):
     student = Required(Student)
     book = Required(Book)
-    creation_date = Required(datetime)
-    max_return_date = Required(datetime)
-    return_date = Optional(datetime)
+    creation_date = Required(datetime.datetime)
+    max_return_date = Required(datetime.datetime)
+    return_date = Optional(datetime.datetime)
 
     return_time = 30
 
@@ -120,27 +81,5 @@ class BookLending(data_source.db.Entity):
             pass
         return self.max_return_date - self.max_return_date
 
-    def get_student(self):
-        return self.student
 
-    def get_book(self):
-        return self.book
-
-    def get_id(self):
-        return self.id
-
-    def get_creation_date(self):
-        return self.creation_date
-
-    def get_max_return_date(self):
-        return self.max_return_date
-
-    def get_return_date(self):
-        if self.return_date is not None:
-            return self.return_date
-
-    def set_return_date(self, return_date):
-        self.return_date = return_date
-
-    def set_creation_date(self, creation_date):
-        self.creation_date = creation_date
+db.generate_mapping(create_tables=True)
