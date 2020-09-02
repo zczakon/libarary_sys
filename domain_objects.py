ed6b88fe2ae0931488326764d6b6e63b569c7109
zczakon <56118290+zczakon@users.noreply.github.com>
2020-09-02 14:50:30 +0200

    refactored Role & Account to domain objects (file contains all db entities)

--- domain_objects.py
+++ domain_objects.py
@@ -1,8 +1,34 @@
 import datetime
+from pony.orm import *
+import random
+import string
 
-from accounts import Account, Role
-from data_source import db
-from pony.orm import Required, Optional, Set
+db = Database()
+# db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+db.bind(provider='sqlite', filename=':memory:')  # TODO bind w mainie i w testach inny - źle, zrobić db.py gdzie są bindy do testów i pliku...
+
+
+class Role(db.Entity):
+    name = Required(str)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Account(db.Entity):
+    role = Required(Role)
+    password = Optional(str)
+
+    def create_password(self):
+        self.password = self.generate_pass(6)
+
+    def change_password(self, new_password):
+        self.password = new_password
+        pass
+
+    @staticmethod
+    def generate_pass(n: int) -> str:
+        return ''.join((random.choice(string.ascii_letters + string.digits) for _ in range(n)))
 
 
 class Student(db.Entity):
