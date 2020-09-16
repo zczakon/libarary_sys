a2a6e3538bea5da5b48c1d9c3e34bc6eadcfed44
zczakon <56118290+zczakon@users.noreply.github.com>
2020-09-16 16:54:49 +0200

    created db schema and some create methods for domain object attributes

--- domain_objects.py
+++ domain_objects.py
@@ -1,77 +1,58 @@
 import datetime
-from pony.orm import *
-import random
-import string
 
-db = Database()
-# db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
-db.bind(provider='sqlite',
-        filename=':memory:')  # TODO bind w mainie i w testach inny - źle, zrobić db.py gdzie są bindy do testów i pliku...
+from sqlalchemy.orm import relationship
+
+from accounts import Account, Role
+import db_bind
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 
 
-class Role(db.Entity):
-    name = Required(str)
-    account = Optional('Account')
+class Student(db_bind.Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    surname = Column(String)
+    pesel = Column(String)
+    registration_date = Column(DateTime)
+    account_id = Column(Integer, ForeignKey('accounts.id'))
+    lending_id = Column(Integer, ForeignKey('lendings.id'))
+
+    account = relationship("Account", back_populates="student")
+    lendings = relationship("BookLending", back_populates="student")
 
     def __str__(self):
-        return str(self.name)
+        return '({}, {}, {}, {})'.format(self.name + ' ' + self.surname, 'PESEL: ' + str(self.pesel),
+                                         'ID:' + str(self.id), 'role: ' + str(self.account.get_role()))
 
+    def __repr__(self):
+        return '({}, {}, {}, {})'.format(self.name + ' ' + self.surname, 'PESEL: ' + str(self.pesel),
+                                         'ID:' + str(self.id), 'role: ' + str(self.account.get_role()))
 
-class Account(db.Entity):
-    role = Required(Role)
-    password = Optional(str)
-    student = Required('Student')
-
-    @db_session
-    def create_password(self):
-        self.set(password=self.generate_pass(6))
-        return self
-
-    @db_session
-    def change_password(self, new_password):
-        self.password = new_password
+    def __eq__(self, other):
         pass
 
-    @db_session
-    def generate_pass(self, n: int) -> str:
-        return ''.join((random.choice(string.ascii_letters + string.digits) for _ in range(n)))
+    def create(self):
+        self.registration_date = datetime.date.today()
+        return self
 
-
-class Student(db.Entity):
-    name = Required(str)
-    surname = Required(str)
-    pesel = Required(str)
-    registration_date = Optional(datetime.date)
-    account = Optional(Account)
-    book_lendings = Set('BookLending')
+    def create_account(self):
+        self.account = Account(Role(name="user"))
+        return self
 
     def fullname(self):
         return str(self.name) + ' ' + str(self.surname)
 
-    @db_session
-    def create_account(self):
-        self.set(account=Account(Role(name="user")))
-        return self
 
-    @db_session
-    def create(self):
-        self.set(registration_date=datetime.date.today())
-        return self
+class Book(db_bind.Base):
+    __tablename__ = 'books'
 
-    def __str__(self):
-        return '({}, {}, {})'.format(self.name + ' ' + self.surname, 'PESEL: ' + self.pesel,
-                                     'ID:' + str(self.id))
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    lending_id = Column(Integer, ForeignKey('lendings.id'))
 
-    def __repr__(self):
-        return '({}, {}, {})'.format(self.name + ' ' + self.surname, 'PESEL: ' + self.pesel,
-                                     'ID:' + str(self.id))
-
-
-class Book(db.Entity):
-    isbn = Required(str)
-    title = Required(str)
-    author = Required(str)
-    book_lendings = Set('BookLending')
+    lendings = relationship("BookLending", back_populates="book")
 
     def __str__(self):
         return '({}, {}, {}, {})'.format(str(self.title), 'author: ' + self.author, 'ISBN: ' +
@@ -82,20 +63,20 @@ class Book(db.Entity):
                                          str(self.isbn), 'ID: ' + str(self.id))
 
 
-class BookLending(db.Entity):
-    student = Required(Student)
-    book = Required(Book)
-    creation_date = Optional(datetime.date)
-    max_return_date = Optional(datetime.date)
-    return_date = Optional(datetime.date)
+class BookLending(db_bind.Base):
+    __tablename__ = 'lendings'
+
+    id = Column(Integer, primary_key=True)
+    creation_date = Column(DateTime)
+    max_return_date = Column(DateTime)
+    return_date = Column(DateTime)
+    student_id = Column(Integer, ForeignKey('students.id'))
+    book_id = Column(Integer, ForeignKey('books.id'))
+
+    student = relationship("Student", back_populates="lendings")
+    book = relationship("Book", back_populates="lendings")
 
     return_time = 30
-
-    @db_session
-    def create(self):
-        self.set(creation_date=datetime.date.today())
-        self.set(max_return_date=self.creation_date + datetime.timedelta(days=self.return_time))
-        return self
 
     def __str__(self):
         return '({}, {}, {}, {})'.format(str(self.book), str(self.student), 'creation date: '
@@ -107,6 +88,15 @@ class BookLending(db.Entity):
                                          + str(self.creation_date), 'return date: ' +
                                          str(self.return_date))
 
+    def create(self):
+        self.creation_date = datetime.date.today()
+        self.max_return_date=self.creation_date + datetime.timedelta(days=self.return_time)
+        return self
+
+    def is_overdue(self):
+        if self.return_date is None:
+            return datetime.date.today() > self.max_return_date
+
     def rental_time(self):
         return datetime.date.today() - self.creation_date
 
@@ -114,6 +104,3 @@ class BookLending(db.Entity):
         if self.return_date is None:
             pass
         return self.max_return_date - self.max_return_date
-
-
-db.generate_mapping(create_tables=True)
