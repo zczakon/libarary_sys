987fe48915ce02f0503f49cebd20012a0f0cd88d
zczakon <56118290+zczakon@users.noreply.github.com>
2020-09-24 12:22:38 +0200

    deleted accounts, unit_tests

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///:memory:', echo=False)  # in memory database
 Base = declarative_base()
+Base.metadata.create_all(engine)
 
 
 
